e7422f897a58909d068c497dc1006eb74b43e0ca
ApexaJ <155083856+ApexaJ@users.noreply.github.com>
2024-02-09 23:41:21 +0530

    Add files via upload

---
+++ simpleintrest.py
@@ -0,0 +1,8 @@
+P = float(input("Enter Principal:"))
+print(P)
+N=float(input("Enter Time:"))
+print(N)
+R=float(input("Enter Intrest Rate:"))
+print(R)
+si=P*N*R/100
+print("Simple Intrest is:",si)
